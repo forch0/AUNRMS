9d45ea354ae64455fc0faf941cc66584873c15df
forch0 <forchh.dev@gmail.com>
2024-07-04 22:55:14 +0100

    migration

--- RMS/UserProfiles/admin.py
+++ RMS/UserProfiles/admin.py
@@ -1,32 +1,33 @@
-# your_app_name/admin.py
-
 from django.contrib import admin
 from .models import UserCred, Resident, Staff, Role
 
 @admin.register(UserCred)
 class UserCredAdmin(admin.ModelAdmin):
-    list_display = ('username', 'email', 'firstname', 'lastname', 'is_staff', 'is_active')
+    list_display = ('id', 'username', 'email', 'firstname', 'lastname', 'is_staff', 'is_active')
     search_fields = ('username', 'email', 'firstname', 'lastname')
     list_filter = ('is_staff', 'is_active')
+    ordering = ('id',)  # Sort by ID in ascending order by default
 
 @admin.register(Resident)
 class ResidentAdmin(admin.ModelAdmin):
-    list_display = ('user',) #'room_id'
+    list_display = ('id', 'user',)
     search_fields = ('user__username',)
-    raw_id_fields = ('user',)  # Improves performance for large databases
+    raw_id_fields = ('user',)
+    ordering = ('id',)  # Sort by ID in ascending order by default
 
 @admin.register(Staff)
 class StaffAdmin(admin.ModelAdmin):
-    list_display = ('user', 'get_role_name')  # display user and related role name
+    list_display = ('id', 'user', 'get_role_name')
     search_fields = ('user__username', 'role__name')
 
     def get_role_name(self, obj):
         return obj.role.name
 
-    get_role_name.short_description = 'Role'  # column header
+    get_role_name.short_description = 'Role'
+    ordering = ('id',)  # Sort by ID in ascending order by default
 
 @admin.register(Role)
 class RoleAdmin(admin.ModelAdmin):
-    list_display = ('name', 'abbreviation')  # display both name and abbreviation
+    list_display = ('id', 'name', 'abbreviation')
     search_fields = ('name', 'abbreviation')
-
+    ordering = ('id',)  # Sort by ID in ascending order by default
--- RMS/UserProfiles/models.py
+++ RMS/UserProfiles/models.py
@@ -72,6 +72,7 @@ class Resident(models.Model):
         verbose_name_plural = 'residents'
 
 class Role(models.Model):
+    id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=20, unique=True)
     abbreviation = models.CharField(max_length=5, blank=True, unique=True)
 
