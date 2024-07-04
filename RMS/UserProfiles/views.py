9c4eed0fcda9fae0d99a4517da3dacd548a44528
forch0 <forchh.dev@gmail.com>
2024-07-04 14:29:29 +0100

    template folder structure

--- RMS/UserProfiles/views.py
+++ RMS/UserProfiles/views.py
@@ -7,21 +7,22 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Welcome to AUN ResLife App.")
+
 class StaffLoginView(LoginView):
-    template_name = 'staff_login.html'
+    template_name = 'userprofiles/staff_login.html'
     success_url = reverse_lazy('staff_dashboard')
 
 class ResidentLoginView(LoginView):
-    template_name = 'resident_login.html'
+    template_name = 'userprofiles/resident_login.html'
     success_url = reverse_lazy('resident_dashboard')
 
 @login_required
 def staff_dashboard(request):
-    return render(request, 'staff_dashboard.html')
+    return render(request, 'userprofiles/staff_dashboard.html')
 
 @login_required
 def resident_dashboard(request):
-    return render(request, 'resident_dashboard.html')
+    return render(request, 'userprofiles/resident_dashboard.html')
 
 class CustomLogoutView(LogoutView):
     next_page = reverse_lazy('login')  # Redirect to login page after logout
