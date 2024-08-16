ada8fdd277093c2a242ef9cd9c1b1aaeed98c773
forch0 <forchh.dev@gmail.com>
2024-08-16 20:50:17 +0100

    Custom authentication modification

--- RMS/UserProfiles/auth_backends.py
+++ RMS/UserProfiles/auth_backends.py
@@ -1,20 +1,23 @@
-from django.contrib.auth.backends import ModelBackend
-from .models import UserCred, Staffs, Residents
+from django.contrib.auth.backends import BaseBackend
+from .models import UserCred, Residents, Enrollment
 
-class StaffBackend(ModelBackend):
-    def authenticate(self, request, username=None, password=None, **kwargs):
+class StaffBackend(BaseBackend):
+    def authenticate(self, request, email=None, password=None, **kwargs):
         try:
-            user = UserCred.objects.get(username=username, is_staff=True)
+            user = UserCred.objects.get(email=email, is_staff=True)
             if user.check_password(password):
                 return user
         except UserCred.DoesNotExist:
             return None
 
-class ResidentBackend(ModelBackend):
-    def authenticate(self, request, username=None, password=None, **kwargs):
+class ResidentBackend(BaseBackend):
+    def authenticate(self, request, email=None, room_number=None, password=None, **kwargs):
         try:
-            user = UserCred.objects.get(username=username, is_staff=False)
+            user = UserCred.objects.get(email=email)
             if user.check_password(password):
+                # Ensure the user is a resident with an active enrollment
+                resident = Residents.objects.get(user=user)
+                Enrollment.objects.get(resident=resident, room__room_number=room_number)
                 return user
-        except UserCred.DoesNotExist:
+        except (UserCred.DoesNotExist, Residents.DoesNotExist, Enrollment.DoesNotExist):
             return None
