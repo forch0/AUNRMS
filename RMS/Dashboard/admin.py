e048ad134a1c1782d01ad377f2b3980af0e77c6c
forch0 <forchh.dev@gmail.com>
2024-09-20 11:16:01 +0100

    Admin site and header change

--- RMS/Dashboard/admin.py
+++ RMS/Dashboard/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
+
+admin.site.site_header = "AUN Residence Management System"
+admin.site.site_title = "AUN Residence Management System"
+admin.site.index_title = "Welcome to AUN Residence Management System"
