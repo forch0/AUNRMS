76d83b7de3e6ca5467d59c4e7a47caaa1f68e8f3
forch0 <forchh.dev@gmail.com>
2024-10-02 13:01:51 +0100

    cache cleaner

--- RMS/RMS/settings.py
+++ RMS/RMS/settings.py
@@ -86,6 +86,7 @@ INSTALLED_APPS = [
     "unfold",
     'admin_tools_stats',  # this must be BEFORE 'admin_tools' and 'django.contrib.admin'
     'django_nvd3',
+    'cache_cleaner'
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
