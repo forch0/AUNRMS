b2915d40139ffc201aae610e3a4120fb17de84a0
forch0 <forchh.dev@gmail.com>
2024-11-15 17:22:17 +0100

    cleaned gabbage

--- RMS/Dashboard/admin.py
+++ RMS/Dashboard/admin.py
@@ -1,5 +1,12 @@
 from django.contrib import admin
 from django.urls import path
+from django.contrib import admin
+from django.urls import path
+from django.shortcuts import redirect
+from django.contrib.auth import get_user_model
+from django.contrib.auth.models import Permission
+# from .views import admin_dashboard
+
 
 admin.site.site_header = "AUN Residence Management System"
 admin.site.site_title = "AUN Residence Management System"
--- RMS/Dashboard/views.py
+++ RMS/Dashboard/views.py
@@ -4,6 +4,17 @@ from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import reverse_lazy
 from django.http import HttpResponse
+from AcademicYear.models import Semester
+from django.utils import timezone
 
 def index(request):
     return HttpResponse("Welcome to Dashboard Section.")
+
+
+# from django.shortcuts import render
+# from .models import Semester
+
+# def admin_dashboard(request):
+#     current_semester = Semester.objects.filter(start_date__lte=timezone.now(), end_date__gte=timezone.now()).first()
+    
+#     return render(request, 'admin/index.html', {'current_semester': current_semester})
